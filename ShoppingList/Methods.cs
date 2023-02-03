13bb8ff5a40eeb4f0c38c9c4172f356ac77053cc
Chris Schmalz <cschmalz03@gmail.com>
2023-02-02 21:02:09 -0500

    initial commit

---
+++ ShoppingList/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList
+{
+    internal class Methods
+    {
+        public static void DisplayGroceries(Dictionary<string, decimal> prices)
+        {
+            Console.WriteLine("Item\tPrice");
+            Console.WriteLine("----------------");
+
+            foreach (KeyValuePair<string, decimal> item in prices)
+            {
+                Console.WriteLine($"{item.Key}\t${item.Value}");
+            }
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,54 @@
+﻿using ShoppingList;
+
+Dictionary<string, decimal> prices = new Dictionary<string, decimal>()
+    {
+        { "apple", 0.99m },
+        { "banana", 0.59m },
+        { "milk", 2.99m },
+        { "bread", 1.99m },
+        { "cheese", 4.99m },
+        { "chicken", 7.99m },
+        { "eggs", 2.99m },
+        { "potato", 0.89m }
+    };
+
+Console.WriteLine("Welcome to Schmalz's Market!\n");
+
+Methods.DisplayGroceries(prices);
+
+
+string response = "yes";
+List<string> receipt = new List<string>();
+while (response == "yes")
+{
+    Console.Write("\nEnter the name of the item you'd like to order: ");
+    string item = Console.ReadLine();
+
+    if (prices.ContainsKey(item))
+    {
+        receipt.Add(item);
+        Console.WriteLine($"\nAdding {item} to cart at ${prices[item]}");
+    }
+    else
+    {
+        Console.WriteLine("\nSorry, we don't have those. Please try again.");
+    }
+
+    Console.Write("\nWould you like to order another item? (yes/no): ");
+    response = Console.ReadLine();
+}
+
+decimal sum = 0;
+Console.WriteLine("\nThanks for your order!");
+Console.WriteLine("Here's what you got:");
+Console.WriteLine("\nItem\tPrice");
+Console.WriteLine("----------------");
+
+foreach (string item in receipt)
+{
+    sum += prices[item];
+    Console.WriteLine($"{item}\t${prices[item]}");
+}
+
+Console.WriteLine($"\nYour total is ${sum}");
+Console.WriteLine("\nThank you for visiting Schmalz's Market!.");
